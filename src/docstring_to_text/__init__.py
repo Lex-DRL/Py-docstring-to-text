7b93677c9c60fda60aa14140936ddfba2cae8681
Lex Darlog (DRL) <3897975+Lex-DRL@users.noreply.github.com>
2025-09-21 10:11:23 +0500

    MPL header in code

--- src/docstring_to_text/__init__.py
+++ src/docstring_to_text/__init__.py
@@ -3,6 +3,10 @@
 A simple pip package converting docstrings into clean text (proper paragraphs and indents).
 """
 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 import typing as _t
 
 from inspect import cleandoc, getdoc
