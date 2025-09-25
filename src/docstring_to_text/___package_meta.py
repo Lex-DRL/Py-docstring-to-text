9ca761131a0e842bc45276cfccf29a3634531e6c
Lex Darlog (DRL) <3897975+Lex-DRL@users.noreply.github.com>
2025-09-25 11:41:35 +0500

    `___package_meta`: VERSION_TUPLE

--- src/docstring_to_text/___package_meta.py
+++ src/docstring_to_text/___package_meta.py
@@ -1,3 +1,21 @@
 # encoding: utf-8
 
+# It should be a hard-coded string, for build tools to detect it:
 VERSION = "1.0.2"
+
+import typing as _t
+
+
+def __version_parts_gen(ver_str: str) -> _t.Generator[_t.Union[int, str], _t.Any, None]:
+	for part_by_dot in ver_str.split("."):
+		for part in part_by_dot.split('-'):
+			if not part:
+				continue
+			try:
+				yield int(part)
+			except Exception:
+				yield part
+
+
+# For actual python code to compare:
+VERSION_TUPLE: _t.Tuple[_t.Union[int, str], ...] = tuple(__version_parts_gen(VERSION))
