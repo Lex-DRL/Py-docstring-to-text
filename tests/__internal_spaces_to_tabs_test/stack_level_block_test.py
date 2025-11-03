6e0676770738817d79ec91223c102a9bc8096fac
Lex Darlog (DRL) <3897975+Lex-DRL@users.noreply.github.com>
2025-11-03 17:14:12 +0500

    wip: `__internal_spaces_to_tabs`

---
+++ tests/__internal_spaces_to_tabs_test/stack_level_block_test.py
@@ -0,0 +1,19 @@
+# encoding: utf-8
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""
+"""
+
+import typing as _t
+from typing import Any as _A, Callable as _C, Optional as _O, Union as _U, Sequence as _S, Iterable as _I
+
+import pytest
+
+from docstring_to_text.__internal_spaces_to_tabs import _StackLevelBlock
+
+
+def test_init():
+	pass
